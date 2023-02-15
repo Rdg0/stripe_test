7e2a46a01100e38bcda03c32a2c74047b08e546f
Malanichev Aleksey <malanichevab@gmail.com>
2023-02-15 19:17:16 +0300

    README

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -14,12 +14,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8fc^by%_3#p)^2j))^m2!46yk+v=_1oqa&=el4n6xbvudx!7zw'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', '194.87.210.36', 'localhost', '127.0.0.1', 'web']
+ALLOWED_HOSTS = []
 
 
 # Application definition
